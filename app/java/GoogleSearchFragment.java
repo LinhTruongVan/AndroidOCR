c6c9db6d3c1c4c742f604cfe25df10e2aff503a3
tvlinh <tvlinh@orientsoftware.asia>
2015-06-13 11:37:25 +0700

    Create Swipable UI with 3 tabs: results , camera , google search

---
+++ app/java/GoogleSearchFragment.java
@@ -0,0 +1,21 @@
+
+package app.com.augmentedreality;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class GoogleSearchFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.google_search_tab, container, false);
+
+        return rootView;
+    }
+
+}
---
+++ app/java/OpenCVCameraFragment.java
@@ -0,0 +1,21 @@
+
+package app.com.augmentedreality;
+        import android.os.Bundle;
+        import android.support.v4.app.Fragment;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+
+
+public class OpenCVCameraFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.opencv_camera_tab, container, false);
+
+        return rootView;
+    }
+
+}
